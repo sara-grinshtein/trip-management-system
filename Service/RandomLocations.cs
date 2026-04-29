dfe3c6bc8d01003c771d5c319dedbf38993286d7
sara-grinshtein <sry3038@gmail.com>
2026-04-30 00:02:46 +0300

    fix RandomLocation

--- Service/RandomLocations.cs
+++ Service/RandomLocations.cs
@@ -14,10 +14,16 @@ namespace Service
     public class RandomLocations
     {
         private readonly IService<TeacherDto> serviceTeacher;
+        private readonly IService<StudentDto> serviceStudent;
 
-        public RandomLocations(IService<TeacherDto> serviceTeacher) 
+        public static List<LocationDto> locations=new List<LocationDto>();
+        public static double dmsPerSecond = 1.4/30;//hoe many dms seconds a man go in our second
+        public int timeToRefresh = 5;
+
+        public RandomLocations(IService<TeacherDto> serviceTeacher, IService<StudentDto> serviceStudent)
         {
             this.serviceTeacher = serviceTeacher;
+            this.serviceStudent = serviceStudent;
         }
 
         //a function that accepts a teacher's ID and returns
@@ -26,39 +32,50 @@ namespace Service
         public async Task<List<LocationDto>> getLocationByTeacherId(string Id)
         {
             var teacher = await serviceTeacher.Getbyid(Id);
-            if (teacher == null)
+            var classTeacher = teacher.classTeacher;
+            var students = await serviceStudent.GetAll();
+            var studentsByTeacher = students.Where(s => s.classStudent == classTeacher).ToList();
+
+            CoordinateDto LatitudePark = new CoordinateDto
             {
-                throw new Exception("there is no teacher with this id.");
-            }
-            var LocationsDto = new List<LocationDto>();
-            var random = new Random();
-            for (int i = 1; i <= 10; i++)
+                Degrees = 32,
+                Minutes = 47,
+                Seconds = 32.6
+            };
+            CoordinateDto LongitudePark = new CoordinateDto
             {
-                //Latitude
-                CoordinateDto Latitude = new CoordinateDto
+                Degrees = 34,
+                Minutes = 57,
+                Seconds = 25.9
+            };
+
+            if (locations.Count == 0)
+            {
+                foreach (var student in studentsByTeacher)
                 {
-                    Degrees = 32,
-                    Minutes = 49 + random.Next(-2, 3),
-                    Seconds = random.Next(0, 60)
-                };
-                CoordinateDto Longitude = new CoordinateDto
-                {
-                    Degrees = 34,
-                    Minutes = 59 + random.Next(-2, 3),
-                    Seconds = random.Next(0, 60)
-                };
-                var Location = new LocationDto
-                {
-                  ID = random.Next(100000, 999999).ToString(),
-                  Latitude = Latitude,
-                  Longitude = Longitude,
-                  Time = DateTime.Now,
-                };
-                LocationsDto.Add(Location);
+                    locations.Add(new LocationDto
+                    {
+                        ID = student.Id,
+                        Latitude = LatitudePark,
+                        Longitude = LongitudePark,
+                        Time = new DateTime()
+                    });
+                }
+                return locations;
+
             }
-            return LocationsDto;
+            Random r = new Random();
+
+            foreach (var location in locations)
+            {
+                var latChange = r.Next(0, Convert.ToInt32(dmsPerSecond * timeToRefresh)); //הגרלה בין 0 לזמן * dmsperSecond
+                var lonChange = r.Next(0, Convert.ToInt32(dmsPerSecond * timeToRefresh)); //הגרלה בין 0 לזמן * dmsperSecond
+
+                location.Latitude.Seconds += latChange;
+                location.Longitude.Seconds += lonChange;
+
+            }
+            return locations;
         }
-
-
     }
 }
